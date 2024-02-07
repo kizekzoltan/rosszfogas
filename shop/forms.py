62664724de62e7d8300595353805cf43532980d3
kizekzoltan <kizekzoltan@gmail.com>
2024-02-07 23:56:38 +0100

    footer megoldva + HIRDETES FELADAS (kepmereten kivul) KESZ

--- shop/forms.py
+++ shop/forms.py
@@ -2,23 +2,26 @@ from django import forms
 
 class HirdetesForm(forms.Form):
     ALLAPOT = [
+        ('--válasszon--', '--Válasszon--'),
         ('bontatlan', 'Bontatlan'),
         ('újszerű', 'Újszerű'),
         ('használt', 'Használt'),
         ('megviselt', 'Megviselt'),
     ]
     KATEGORIA = [
+        ('--válasszon--', '--Válasszon--'),
         ('autó', 'Autó'),
         ('élelmiszer', 'Élelmiszer'),
         ('egyéb', 'Egyéb'),
     ]
 
-    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Termék'}))
-    price = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Ár'}))
-    feladocim = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'input-field', 'placeholder': 'pl.: 9012 Győr, Kalapos utca 6.'}))
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'pl: Samsung Galaxy S21'}))
+    price = forms.CharField(max_length=10, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'pl: 15000'}))
+    feladocim = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'pl: 9012 Győr, Kalapos utca 12.'}))
+    feladoorszag = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'pl: Magyarország'}))
     kategoria = forms.ChoiceField(choices=KATEGORIA)
     allapot = forms.ChoiceField(choices=ALLAPOT)
-    leiras = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Leírás'}))
+    leiras = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': ''}))
     kep = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple':False}))
     terms_checkbox = forms.BooleanField(required=True)
 
