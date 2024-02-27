9f97e57f274d2dc539d6a1ac82f5326430f1ee5c
kizekzoltan <kizekzoltan@gmail.com>
2024-02-27 18:44:30 +0100

    asd

--- shop/views.py
+++ shop/views.py
@@ -19,6 +19,7 @@ def hirdetes_letrehozasa(request):
     if request.method == 'POST':
         form = HirdetesForm(request.POST, request.FILES)
         if form.is_valid():
+
             product = Product(
                 name=form.cleaned_data['name'],
                 price=form.cleaned_data['price'],
@@ -29,16 +30,24 @@ def hirdetes_letrehozasa(request):
                 kategoria = form.cleaned_data['kategoria'],
                 customer=request.user.customer
             )
-            if product.price < 1:
-                form.add_error('price', "Az ár nem lehet kevesebb 1 HUF-nál!")
+            
+            if 'image' not in request.FILES:
+                form.add_error('image', "Képet kötelező feltölteni!")
                 return render(request, 'shop/hirdetes_letrehozasa.html', {'form': form})
             
+            if product.price < 1:
+                form.add_error('price', "Az ár nem lehet kisebb 1 HUF-nál!")
+                return render(request, 'shop/hirdetes_letrehozasa.html', {'form': form})
+            
+            if product.price > 100000000:
+                form.add_error('price', "Az ár nem lehet nagyobb 100.000.000 HUF-nál!")
+                return render(request, 'shop/hirdetes_letrehozasa.html', {'form': form})
 
             extension = product.image.name.split('.')[-1].lower()
             if extension == 'gif':
                 form.add_error('image', "Nem tölthetsz fel gif-et Söli!")
                 return render(request, 'shop/hirdetes_letrehozasa.html', {'form': form})
-            
+
             else:
                 product.save()
                 return redirect('fiok') 
